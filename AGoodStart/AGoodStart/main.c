41f292640725ab5a8fb32f0df50a8c996063374d
ZeyuWei <you@example.com>
2016-01-20 01:28:35 +0800

    First exercise!

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by hemster on 1/20/16.
+//  Copyright © 2016 Zeyu Wei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
